c28793c5d6e443177721828d798b67568b4153c6
Shriyansh Mishra <shriyansh925@gmail.com>
2025-06-27 15:41:13 +0530

    removed bearer token

--- logging-middleware/logApi.ts
+++ logging-middleware/logApi.ts
@@ -29,7 +29,7 @@ export async function logApi(
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJzaHJpeWFuc2guMjJzY3NlMTAxMTM0NEBnYWxnb3RpYXN1bml2ZXJzaXR5LmVkdS5pbiIsImV4cCI6MTc1MTAxMzk0MCwiaWF0IjoxNzUxMDEzMDQwLCJpc3MiOiJBZmZvcmQgTWVkaWNhbCBUZWNobm9sb2dpZXMgUHJpdmF0ZSBMaW1pdGVkIiwianRpIjoiZmI3MzVjN2EtOTYzZi00YjlhLWFmZTktYTlmN2MxNjAxYzI3IiwibG9jYWxlIjoiZW4tSU4iLCJuYW1lIjoic2hyaXlhbnNoIG1pc2hyYSIsInN1YiI6ImI2ZDk4M2ZmLTZkZWYtNDU1Ni1iODUzLTQ0NjYwNGJlYWFlOCJ9LCJlbWFpbCI6InNocml5YW5zaC4yMnNjc2UxMDExMzQ0QGdhbGdvdGlhc3VuaXZlcnNpdHkuZWR1LmluIiwibmFtZSI6InNocml5YW5zaCBtaXNocmEiLCJyb2xsTm8iOiIyMnNjc2UxMDExMzQ0IiwiYWNjZXNzQ29kZSI6Ik11YWd2cSIsImNsaWVudElEIjoiYjZkOTgzZmYtNmRlZi00NTU2LWI4NTMtNDQ2NjA0YmVhYWU4IiwiY2xpZW50U2VjcmV0IjoiZWFXUnJYbWdYQ3NiSENtUSJ9.vseAu_DPP9u1La2Z-YhjZZ-s4KmAGWm9skefhY9ydg4'
+        'Authorization': 'Bearer'
       },
       body: JSON.stringify(body),
     });
